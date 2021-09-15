45ad850df0d7bff5a0b6a7d9eb3476725b7d4b33
Deborah Otuagomah <thatgirldorian@gmail.com>
2021-09-16 00:02:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+//function to play on hover and pause when mouse is out
+
+function hover(element) {
+    element.play(); 
+}
+
+function out(element) {
+    element.pause();
+}
